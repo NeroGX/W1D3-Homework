94732edb3fe10ae088da0369e01e7b7553c8df62
Joseph Robinson <josephr1981@live.com>
2022-07-26 01:14:44 -0400

    Added loops.js and completed the current week homework.

---
+++ loops.js
@@ -0,0 +1,68 @@
+/* 1. Creates a loop that goes from 0-99 in ASCENDING order
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+}
+*/
+
+/* 2. Creates a loop that logs 99-0 in DESCENDING order
+for (let i = 99; i >= 0; i--) {
+    console.log(i);
+}
+*/
+
+/* 3. Creates a loop that logs EVEN numbers from 0-98 in ASCENDING order
+for (let i = 0; i < 99; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+*/
+
+/* 4. Creates a loop that logs EVEN numbers from 90-0 in DESCENDING order
+for (let i = 98; i >= 0; i--) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+*/
+
+/* 5. Creates a loop that logs ODD numbers from 0-99 in ASCENDING order
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+*/
+
+/* 6. Creates a loop that logs ODD numbers from 99-0 in DESCENDING order
+for (let i = 99; i >= 0; i--) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+*/
+
+/* 7. Creates a loop that logs from 49-72 in ASCENDING order
+for (let i = 49; i < 73; i++) {
+    console.log(i);
+}
+*/
+
+/* 8. Creates a loop that logs from 81-26 in DESCENDING order
+for (let i = 81; i > 25; i--) {
+    console.log(i);
+}
+*/
+
+/* 9. Creates a loop that logs 3-90 in multiples of 3
+for (let i = 3; i <=90; i += 3) {
+    console.log(i);
+}
+*/
+
+/* Programmer's note. You will have to open the html folder
+and uncomment which one you need to test, but all of them
+should work as expected. Let me know if there are any errors
+or inconsistencies.
+-Joseph Robinson(NeroGX)
+*/
